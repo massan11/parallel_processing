025970689b10a5089e2f3e1f064ffcb5da94e4de
massan11 <mhassan.zoho@gmail.com>
2025-02-04 23:08:07 +0330

    Add multithreading example with sum calculations

---
+++ thread_01.py
@@ -0,0 +1,21 @@
+import threading
+
+def sum100():
+    sum = 0
+    for i in range(1, 101):
+        sum += i
+    print(sum)
+    
+def sum200():
+    sum = 0
+    for i in range(1, 201):
+        sum += i
+    print(sum)
+    
+sum_1 = threading.Thread(target=sum100, name="sum_100")
+sum_2 = threading.Thread(target=sum200, name="sum_200")
+
+sum_1.start()
+sum_2.start()
+    
+# sum100()
