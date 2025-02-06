e5dc2ca7cabd7968f682e32c5bcd2c33c4558f44
massan11 <mhassan.zoho@gmail.com>
2025-02-06 08:49:17 +0330

    Refactor counter implementation to use a class with thread-safe incrementing

--- thread_06.py
+++ thread_06.py
@@ -1,31 +1,37 @@
 import threading
 
-count = 0
-
-def increase():
-    global count
+class Counter:
+    def __init__(self):
+        self.value = 0
+        self._lock = threading.Lock()
+        
+    def increase(self):
+        self._lock.acquire()
+        self.value += 1
+        self._lock.release()
+            
+            
+def increase(count):
     for _ in range(500000):
-        count += 1
+        count.increase()
+            
+    
         
 def count_money():
-    global count
-    count = 0
     
-    th_1 = threading.Thread(target=increase)
-    th_2 = threading.Thread(target=increase)
-    th_3 = threading.Thread(target=increase)
-    th_4 = threading.Thread(target=increase)
+    count = Counter()
+    
+    th_1 = threading.Thread(target=increase, args=(count, ))
+    th_2 = threading.Thread(target=increase, args=(count, ))
     
     th_1.start()
     th_2.start()
-    th_3.start()
-    th_4.start()
     
-    # th_1.join()
+    th_1.join()
     th_2.join()
-    th_3.join()
-    th_4.join()
+       
+    return count
 
 for index in range(1, 6):
-    count_money()
-    print(f"count must be {4*500000} but it is {count}" )
+    count = count_money()
+    print(f"count must be {500000*2} but it is {count.value}" )
